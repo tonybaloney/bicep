3421fa49bb486999a7856cf392ce3f9ad64f23b6
Marcin Jastrzebski <majastrz@microsoft.com>
2022-08-09 20:08:42 -0700

    fixed bicep exception

--- src/Bicep.Cli/Commands/RootCommand.cs
+++ src/Bicep.Cli/Commands/RootCommand.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using Bicep.Cli.Arguments;
+using Bicep.Core.Exceptions;
 using System;
 using System.IO;
 using System.IO.Compression;
@@ -174,7 +175,7 @@ Usage:
         private static void WriteEmbeddedResource(TextWriter writer, string streamName)
         {
             using var stream = typeof(RootCommand).Assembly.GetManifestResourceStream(streamName)
-                ?? throw new InvalidOperationException($"The resource stream '{streamName}' is missing from this executable. Please use an official build of this executable to access the requested information.");
+                ?? throw new BicepException($"The resource stream '{streamName}' is missing from this executable. Please use an official build of this executable to access the requested information.");
 
             using var decompressor = new DeflateStream(stream, CompressionMode.Decompress);
 
